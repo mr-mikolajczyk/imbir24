25ac3a8d455562fcdc068deefd79a74cada05a49
mr-mikolajczyk <159031258+mr-mikolajczyk@users.noreply.github.com>
2024-08-20 16:34:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,102 +1,114 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.querySelector('body');
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.querySelector("body");
 
 const setTheme = (theme) => {
   body.dataset.theme = theme;
-  localStorage.setItem('preferred-theme', theme);
+  localStorage.setItem("preferred-theme", theme);
 };
 
 const getTheme = () => {
-  const theme = localStorage.getItem('preferred-theme');
-  return theme || 'dark'; // domyślny motyw
+  const theme = localStorage.getItem("preferred-theme");
+  return theme || "dark"; // domyślny motyw
 };
 
 const toggleTheme = () => {
   const currentTheme = body.dataset.theme;
-  const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+  const newTheme = currentTheme === "light" ? "dark" : "light";
   setTheme(newTheme);
 };
 
-toggleButton.addEventListener('click', toggleTheme);
+toggleButton.addEventListener("click", toggleTheme);
 
 // Ustawienie domyślnego motywu z localStorage
 const preferredTheme = getTheme();
 setTheme(preferredTheme);
 
 // use a script tag or an external JS file
- document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(Flip, ScrollTrigger)
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(Flip, ScrollTrigger);
   //theme
-  
+
   //navbar
-const showAnim = gsap
-.from(".nav_component", {
-  yPercent: -150,
-  opacity:0,
-  paused: true,
-  duration: 0.8,
-  ease: "power3.inOut",
-})
-.progress(1);
-ScrollTrigger.create({
-start: "top top",
-end: 99999,
-onUpdate: (self) => {
-  self.direction === -1 ? showAnim.play() : showAnim.reverse();
-},
-});
-gsap.from(".nav_item", {
-yPercent: -100,
-opacity: 0,
-stagger: 0.1,
-});
+  const showAnim = gsap
+    .from(".nav_component", {
+      yPercent: -150,
+      opacity: 0,
+      paused: true,
+      duration: 0.8,
+      ease: "power3.inOut",
+    })
+    .progress(1);
+  ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+      self.direction === -1 ? showAnim.play() : showAnim.reverse();
+    },
+  });
+  gsap.from(".nav_item", {
+    yPercent: -100,
+    opacity: 0,
+    stagger: 0.1,
+  });
 
-let navbarLogoWrapper = document.querySelector('.navbar_logo_wrapper').offsetHeight;
-let startPosition = 'top+=' + navbarLogoWrapper + 'px' + ' top';
-function navChange() {
-navbarLogoWrapper = document.querySelector('.navbar_logo_wrapper').offsetHeight;
-startPosition = 'top+=' + navbarLogoWrapper + 'px' + ' top';
-ScrollTrigger.refresh();
-console.log(startPosition)
-};
-window.addEventListener("resize", navChange);
-const tlNav = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".navbar_logo_wrapper",
-    start: startPosition,
-    end: "bottom center",
-    invalidateOnRefresh: true,
-    toggleActions: "play none reverse reset"
+  let navbarLogoWrapper = document.querySelector(
+    ".navbar_logo_wrapper"
+  ).offsetHeight;
+  let startPosition = "top+=" + navbarLogoWrapper + "px" + " top";
+  function navChange() {
+    navbarLogoWrapper = document.querySelector(
+      ".navbar_logo_wrapper"
+    ).offsetHeight;
+    startPosition = "top+=" + navbarLogoWrapper + "px" + " top";
+    ScrollTrigger.refresh();
+    console.log(startPosition);
   }
-})
-.to('.slogan', {
-  opacity:0,
-},0)
-.to('.logonav.small', {
-  opacity:1,
-},0);
+  window.addEventListener("resize", navChange);
+  const tlNav = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".navbar_logo_wrapper",
+        start: startPosition,
+        end: "bottom center",
+        invalidateOnRefresh: true,
+        toggleActions: "play none reverse reset",
+      },
+    })
+    .to(
+      ".slogan",
+      {
+        opacity: 0,
+      },
+      0
+    )
+    .to(
+      ".logonav.small",
+      {
+        opacity: 1,
+      },
+      0
+    );
 
-//Cursor
-  const cursor = document.querySelector('.cursor');
-  const cursorStroke = document.querySelector('.cursor_stroke');
-  const linksAndButtons = document.querySelectorAll('a, button');
-  const cursorShowreel = document.querySelector('.showreel_cursor');
-  const showreel = document.querySelector('.showreel');
-  const showreelVideo = document.querySelector('#showreel-video');
+  //Cursor
+  const cursor = document.querySelector(".cursor");
+  const cursorStroke = document.querySelector(".cursor_stroke");
+  const linksAndButtons = document.querySelectorAll("a, button");
+  const cursorShowreel = document.querySelector(".showreel_cursor");
+  const showreel = document.querySelector(".showreel");
+  const showreelVideo = document.querySelector("#showreel-video");
 
-  const showreelHero = document.querySelector('.hero_showreel');
-
+  const showreelHero = document.querySelector(".hero_showreel");
 
   showreel.addEventListener("click", () => {
     showreelVideo.muted = !showreelVideo.muted;
-    const state = Flip.getState(".hero_showreel");  
+    const state = Flip.getState(".hero_showreel");
     showreelHero.classList.toggle("is-open");
     body.classList.toggle("overflow");
     cursorShowreel.classList.toggle("close");
 
     Flip.from(state, {
-      absolute: true, 
-      duration: 0.5, 
+      absolute: true,
+      duration: 0.5,
       ease: "power3.out",
     });
   });
@@ -110,63 +122,70 @@ const tlNav = gsap.timeline({
       cursorShowreel.classList.remove("close"); // Restore cursor styles
 
       Flip.from(state, {
-      absolute: true, 
-      duration: 0.5, 
-      ease: "power3.out",
-    });
+        absolute: true,
+        duration: 0.5,
+        ease: "power3.out",
+      });
     }
   });
-  
 
-  gsap.set([cursor, cursorStroke, cursorShowreel], { xPercent: -50, yPercent: -50 });
+  gsap.set([cursor, cursorStroke, cursorShowreel], {
+    xPercent: -50,
+    yPercent: -50,
+  });
 
   const xC = gsap.quickSetter(cursor, "x", "px");
   const yC = gsap.quickSetter(cursor, "y", "px");
   const xCS = gsap.quickSetter(cursorShowreel, "x", "px");
   const yCS = gsap.quickSetter(cursorShowreel, "y", "px");
 
-  window.addEventListener("mousemove", e => {
-    xC(e.x)
-    yC(e.y)
-    xCS(e.x)
-    yCS(e.y)
+  window.addEventListener("mousemove", (e) => {
+    xC(e.x);
+    yC(e.y);
+    xCS(e.x);
+    yCS(e.y);
     gsap.to(cursorStroke, {
       duration: 0.2,
       x: e.clientX,
-      y: e.clientY
-    })
+      y: e.clientY,
+    });
   });
 
-  const handleHover = (element, opacityOnHover, scaleOnHover, duration = 0.4, ease = "power3.inOut") => {
-    element.addEventListener('mouseover', () => {
+  const handleHover = (
+    element,
+    opacityOnHover,
+    scaleOnHover,
+    duration = 0.4,
+    ease = "power3.inOut"
+  ) => {
+    element.addEventListener("mouseover", () => {
       cursor.style.opacity = opacityOnHover;
       cursorStroke.style.opacity = opacityOnHover;
       gsap.to(cursorStroke, { scale: scaleOnHover, duration, ease });
     });
-  
-    element.addEventListener('mouseout', () => {
+
+    element.addEventListener("mouseout", () => {
       cursor.style.opacity = 1;
       cursorStroke.style.opacity = 1;
       gsap.to(cursorStroke, { scale: 1, duration, ease });
     });
   };
-  
-linksAndButtons.forEach(element => handleHover(element, 0, 1.75)); // Default for links and buttons
-showreel.addEventListener('mouseover', () => handleHover(showreel, 0, 2.25)); // Specific behavior for showreel
-document.addEventListener('mousedown', function(event) { 
-  gsap.to(cursorStroke,{
-    scale: 1.5,
-    ease: "power2.in"
-  });
-});
-document.addEventListener('mouseup', function(event) {
-  gsap.to(cursorStroke,{
-    scale:1,
-    delay:0.3,
-    ease: "power2.out"
-  });
-});
 
+  linksAndButtons.forEach((element) => handleHover(element, 0, 1.75)); // Default for links and buttons
+  showreel.addEventListener("mouseover", () => handleHover(showreel, 0, 2.25)); // Specific behavior for showreel
+  document.addEventListener("mousedown", function (event) {
+    gsap.to(cursorStroke, {
+      scale: 1.5,
+      ease: "power2.in",
+    });
+  });
+  document.addEventListener("mouseup", function (event) {
+    gsap.to(cursorStroke, {
+      scale: 1,
+      delay: 0.3,
+      ease: "power2.out",
+    });
+  });
 
   let showreelAnim = gsap.to(cursorShowreel, {
     paused: true,
@@ -178,56 +197,60 @@ document.addEventListener('mouseup', function(event) {
   showreel.addEventListener("mouseenter", () => showreelAnim.play());
   showreel.addEventListener("mouseleave", () => showreelAnim.reverse());
 
-
-
-
   let offerItems = gsap.utils.toArray(".home_portfolio_item_wrapper");
   let scrollItems = gsap.utils.toArray("[data-item='scroll']");
   let offerCards = offerItems.length;
   let card = document.querySelector(".home_portfolio_item_wrapper");
 
   let moveDistance = card.offsetWidth;
-  let move = moveDistance * (offerCards);
-
+  let move = moveDistance * offerCards;
 
   ScrollTrigger.matchMedia({
     // desktop
     "(min-width: 800px)": function () {
+      let offerItems = gsap.utils.toArray(".home_portfolio_item_wrapper");
+      let scrollItems = gsap.utils.toArray("[data-item='scroll']");
+      let offerCards = offerItems.length;
+      let card = document.querySelector(".home_portfolio_item_wrapper");
+      let moveDistance = card.offsetWidth;
+      let move = moveDistance * offerCards;
+
       let scrollTween = gsap.to(".home_portfolio_component", {
-        x: -move,
+        x: -(card.offsetWidth * offerCards),
         ease: "none",
         scrollTrigger: {
           trigger: ".section_home_portfolio",
           start: "center center",
           end: () => "+=" + window.innerHeight,
           pin: true,
-          scrub: 1.5
-        }
+          scrub: 1.5,
+        },
+        invalidateOnRefresh: true,
       });
-    
+
       scrollItems.forEach((card, i) => {
         gsap.from(card, {
           opacity: 0,
-          yPercent:15,
-          scale:0.9,
+          yPercent: 15,
+          scale: 0.9,
           scrollTrigger: {
             containerAnimation: scrollTween,
             trigger: card,
             start: "left 70%",
             ease: "power3.inOut",
-            toggleActions: "play none none reverse"
-          }
+            toggleActions: "play none none reverse",
+          },
         });
       });
-    }
+    },
   });
 
+  const splitAbout = new SplitType("#imbir-info", {
+    types: "words, chars",
+  });
 
-  const splitAbout = new SplitType('#imbir-info', {
-    types: 'words, chars'
-  })
-
-  const tlInfo = gsap.timeline({
+  const tlInfo = gsap
+    .timeline({
       scrollTrigger: {
         trigger: ".section_home_about",
         start: "top 80%",
@@ -235,83 +258,80 @@ document.addEventListener('mouseup', function(event) {
         // end: "+=150%",
         // pin: true,
         scrub: 0.5,
-      }
+      },
     })
     .from(splitAbout.chars, {
-      opacity: .1,
+      opacity: 0.1,
       scale: 0.95,
-        stagger: 0.01,
-        yPercent:10,
-        duration: 0.1,
+      stagger: 0.01,
+      yPercent: 10,
+      duration: 0.1,
     });
 
-    const splitCTA = new SplitType('.heading_cta', {
-      types: 'words, chars'
+  const splitCTA = new SplitType(".heading_cta", {
+    types: "words, chars",
+  });
+
+  const tlCTA = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".section_main_cta",
+        start: "top center",
+        end: "bottom 80%",
+        scrub: 2,
+      },
     })
-  
-    const tlCTA = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".section_main_cta",
-          start: "top center",
-          end: "bottom 80%",
-          scrub: 2,
-        }
-      })
-      .from(splitCTA.chars, {
-        opacity: 0.01,
-        scale: 0.90,
-        stagger: 0.01,
-        yPercent:10,
-        duration: 0.1,
-      });
+    .from(splitCTA.chars, {
+      opacity: 0.01,
+      scale: 0.9,
+      stagger: 0.01,
+      yPercent: 10,
+      duration: 0.1,
+    });
 
-
-
-  gsap.to('.home_photo_component', {
+  gsap.to(".home_photo_component", {
     scrollTrigger: {
-      trigger: '.home_photo_component',
-      scrub: 1
+      trigger: ".home_photo_component",
+      scrub: 1,
     }, // start the animation when ".box" enters the viewport (once)
-    xPercent: -20
+    xPercent: -20,
   });
 
   // Reveal
-gsap.utils
-.toArray(
-  "h2, h3, p, .w-richtext, .w-richtext > *, .w-richtext > * > p, [data-bottom], .w-richtext:not([data-no-animation])"
-)
-.forEach((el, i) => {
-  let projectList = gsap.timeline({
-    scrollTrigger: {
-      trigger: el
-      // start: "top, bottom"
-      // markers: true
-    }
-  });
-  projectList.from(el, {
+  gsap.utils
+    .toArray(
+      "h2, h3, p, .w-richtext, .w-richtext > *, .w-richtext > * > p, [data-bottom], .w-richtext:not([data-no-animation])"
+    )
+    .forEach((el, i) => {
+      let projectList = gsap.timeline({
+        scrollTrigger: {
+          trigger: el,
+          // start: "top, bottom"
+          // markers: true
+        },
+      });
+      projectList.from(el, {
+        y: 50,
+        opacity: 0,
+        duration: 0.6,
+        ease: Power1.easeInOut,
+      });
+    });
+  // Stagger component
+  gsap.set("[data-stagger]", {
     y: 50,
     opacity: 0,
-    duration: 0.6,
-    ease: Power1.easeInOut
   });
-});
-// Stagger component
-gsap.set("[data-stagger]", {
-y: 50,
-opacity: 0
-});
-ScrollTrigger.batch("[data-stagger]", {
-start: "center bottom",
-onEnter: (batch) =>
-  gsap.to(batch, {
-    y: 0,
-    opacity: 1,
-    duration: 0.6,
-    stagger: 0.2,
-    ease: Power1.easeInOut
-  })
-});
+  ScrollTrigger.batch("[data-stagger]", {
+    start: "center bottom",
+    onEnter: (batch) =>
+      gsap.to(batch, {
+        y: 0,
+        opacity: 1,
+        duration: 0.6,
+        stagger: 0.2,
+        ease: Power1.easeInOut,
+      }),
+  });
   // gsap code here!
 });
-
-
